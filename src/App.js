190d14db774a40c600e1d3c96f8656304be5ce52
Aek <ekkawin.v@gmail.com>
2021-08-14 16:51:03 +0700

    push

--- src/App.js
+++ src/App.js
@@ -5,60 +5,63 @@ import NavHeader from "./components/common/NavHeader";
 import WebsiteFooter from "./components/common/WebsiteFooter";
 import HomePage from "./components/home/HomePage";
 
-import {Layout} from "antd";
-import {Switch, Route, useHistory} from "react-router-dom";
-import {useState, useEffect} from "react";
-import {LoginPage} from "./components/login/LoginPage";
-import {RegisterPage} from "./components/login/RegisterPage";
-import {ChoosePackagePage} from "./components/choose-package/ChoosePackagePage";
+import { Layout } from "antd";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { LoginPage } from "./components/login/LoginPage";
+import { RegisterPage } from "./components/login/RegisterPage";
+import { ChoosePackagePage } from "./components/choose-package/ChoosePackagePage";
 
 const App = () => {
-    const history = useHistory();
-    const [isLogin, setIsLogin] = useState(false);
+  const history = useHistory();
+  const [isLogin, setIsLogin] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener('storage', () => {
-            setIsLogin(localStorage.getItem("isLogin"));
-        });
-    }, []);
+  useEffect(() => {
+    window.addEventListener("storage", () => {
+      setIsLogin(localStorage.getItem("isLogin"));
+    });
+  }, []);
 
-    useEffect(() => {
-        if (!isLogin) {
-            history.push("/login");
-        } else {
-            setIsLogin(true);
-        }
-    }, [isLogin])
-
+  useEffect(() => {
     if (!isLogin) {
-        return (
-            <>
-                <Route exact path="/login">
-                    <LoginPage setIsLogin={setIsLogin}/>
-                </Route>
-                <Route exact path="/register">
-                    <RegisterPage setIsLogin={setIsLogin}/>
-                </Route>
-            </>
-        );
+      history.push("/login");
+    } else {
+      setIsLogin(true);
     }
+  }, [isLogin]);
 
+  if (!isLogin) {
     return (
-        <>
-            <Switch>
-                <Layout className="layout">
-                    <NavHeader/>
-                    <Route exact path="/">
-                        <HomePage/>
-                    </Route>
-                    <Route exact path="/create-package">
-                        <ChoosePackagePage/>
-                    </Route>
-                    <WebsiteFooter/>
-                </Layout>
-            </Switch>
-        </>
+      <>
+        <Route exact path="/login">
+          <LoginPage setIsLogin={setIsLogin} />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage setIsLogin={setIsLogin} />
+        </Route>
+        <Route exact path="/home">
+          <div>aek</div>
+        </Route>
+      </>
     );
+  }
+
+  return (
+    <>
+      <Switch>
+        <Layout className="layout">
+          <NavHeader />
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/create-package">
+            <ChoosePackagePage />
+          </Route>
+          <WebsiteFooter />
+        </Layout>
+      </Switch>
+    </>
+  );
 };
 
 export default App;
--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -1,29 +1,57 @@
 import "antd/dist/antd.css";
 
-import { Button, Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Button, Layout, Menu, Drawer } from "antd";
+import { Link, useHistory } from "react-router-dom";
 import { PAGE } from "../choose-package/ChoosePackagePage";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../logo.svg";
+import { MenuOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
 
 function NavHeader() {
+  const history = useHistory();
+  const [isVisible, setisVisible] = useState(false);
   return (
-    <Header className="bg-yellow-400 pd-1">
+    <div className="bg-yellow-400 h-16 p-2">
       <div className="flex justify-between items-center h-full">
         <img className="w-2/6 " src={logo} alt="FoodAround" />
-        <Button
-          className="bg-yellow-400 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
+        <div className="flex justify-end items-center">
+          <Button
+            className="bg-yellow-400 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
+            onClick={() => {
+              localStorage.setItem("isLogin", false);
+              window.location.href = "/";
+            }}
+          >
+            Logout
+          </Button>
+          <MenuOutlined
+            style={{ marginLeft: "1rem", color: "white" }}
+            onClick={() => {
+              setisVisible(true);
+            }}
+          />
+        </div>
+      </div>
+      <Drawer
+        placement="right"
+        onClose={() => {
+          setisVisible(false);
+        }}
+        visible={isVisible}
+      >
+        <div
           onClick={() => {
-            localStorage.setItem("isLogin", false);
-            window.location.href = "/";
+            setisVisible(false);
+            history.push("/home");
           }}
         >
-          Logout
-        </Button>
-      </div>
-    </Header>
+          หน้าแรก
+        </div>
+        <div>เปลี่ยนเป็นร้านค้า</div>
+      </Drawer>
+    </div>
   );
 }
 
