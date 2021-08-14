1f8b9104b438a0c75c7d664e6bb4081957d43b84
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2021-08-14 17:24:34 +0700

    form validate payment

--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -11,12 +11,12 @@ const {Header} = Layout;
 
 function NavHeader() {
     return (
-        <Header className="bg-yellow-400 pd-1">
+        <Header className="bg-yellow-400 pd-1 md:py-12">
             <div className="flex justify-between items-center h-full">
-                <img className="w-2/6 " src={logo} alt="FoodAround"/>
+                <img className="w-3/6 md:w-2/6 max-w-xs" src={logo} alt="FoodAround"/>
                 <Button
                     icon={<LogoutOutlined />}
-                    className="bg-yellow-500 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
+                    className="bg-yellow-500 hover:bg-color-600 cursor-pointer text-white border-black rounded-md hover:bg-yellow-600 flex items-center"
                     onClick={() => {
                         localStorage.setItem("isLogin", false);
                         window.location.href = "/";
--- src/components/payment/PaymentPage.js
+++ src/components/payment/PaymentPage.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { Button, Form, Input } from "antd";
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import { css } from "@emotion/react";
 import { PAGE } from "../choose-package/ChoosePackagePage";
 import { PrimaryButton } from "../PrimaryButton";
@@ -9,6 +9,17 @@ import { LeftOutlined } from "@ant-design/icons";
 export const PaymentPage = ({ setPage }) => {
   const [form] = Form.useForm();
   const [paymentMethod, setPaymentMethod] = useState(false);
+  const [formData , setFormData] = useState({cardNumber:"",cardExpire:"",cvv:"",cardHolder:""})
+
+  let changeHandle = (e) =>{
+    e.preventDefault()
+    setFormData({...formData,[e.target.name]:e.target.value})
+    console.log("Yes")
+  }
+
+  useEffect(()=>{
+    console.log(formData)
+  },[formData])
 
   return (
     <div className="w-full h-full text-2xl px-5 py-12">
@@ -48,20 +59,20 @@ export const PaymentPage = ({ setPage }) => {
         <div className="mb-8 mt-8 text-base">
           <Form form={form}>
             หมายเลขบัตร
-            <Form.Item name="email">
-              <Input type="email" placeholder="Email address" />
+            <Form.Item name="cardNumber">
+              <Input type="text" placeholder="Card number" value={form.cardNumber} name="cardNumber" onChange={(e)=> changeHandle(e)}/>
             </Form.Item>
             <div className="flex space-x-3 mt-8">
               <div>
                 บัตรหมดอายุ
-                <Form.Item name="password">
-                  <Input type="password" placeholder="Password" />
+                <Form.Item name="cardExpire">
+                  <Input type="text" placeholder="Card expires date" name="cardExpire" onChange={(e)=> changeHandle(e)}/>
                 </Form.Item>
               </div>
               <div>
                 รหัสหลังบัตร
                 <Form.Item name="cvv">
-                  <Input placeholder="cvv" />
+                  <Input placeholder="cvv" name="cvv" onChange={(e)=> changeHandle(e)}/>
                 </Form.Item>
               </div>
             </div>
@@ -69,7 +80,7 @@ export const PaymentPage = ({ setPage }) => {
               ชื่อบนบัตร (หากไม่มี กรุณาใส่ชื่อ-นามสกุล)
             </div>
             <Form.Item name="cardHolder">
-              <Input placeholder="card holder" />
+              <Input name="cardHolder" placeholder="Card holder name" onChange={(e)=> changeHandle(e)}/>
             </Form.Item>
           </Form>
         </div>
@@ -83,12 +94,12 @@ export const PaymentPage = ({ setPage }) => {
         ชำระเงิน
       </PrimaryButton>
       <div
-        className="flex justify-center items-center text-lg mt-2 cursor-pointer"
+        className="flex justify-center items-center text-xl mt-2 cursor-pointer"
         onClick={() => {
           setPage(PAGE.CHOOSE_PACKAGE);
         }}
       >
-        <LeftOutlined style={{ fontSize: "12px" }} /> ย้อนกลับ
+        <LeftOutlined style={{ fontSize: "14px" }} /> ย้อนกลับ
       </div>
     </div>
   );
