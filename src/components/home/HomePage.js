d6f1f84f4114548b4f41f7deb2f4927a56e12c3c
Aek <ekkawin.v@gmail.com>
2021-08-14 10:51:18 +0700

    set tailwind and title

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,20 +1,20 @@
-import "antd/dist/antd.css";
-
-import { Layout, Breadcrumb } from "antd";
+import { Button, Layout, Breadcrumb } from "antd";
 
 const { Content } = Layout;
 
 function Home() {
-  return (
-    <Content style={{ padding: "0 50px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">Content</div>
-    </Content>
-  );
+    return (
+        <Content style={{ padding: "0 50px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-content">Content</div>
+            <Button className="bg-primary text-primary">hi</Button>
+            <div className="text-yellow-400">aek</div>
+        </Content>
+    );
 }
 
 export default Home;
