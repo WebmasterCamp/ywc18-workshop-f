51b24217301d3bae6a92f03d92fccfc2d9871c08
Intouch Yutinatorn <keyeducation2544@gmail.com>
2021-08-14 15:49:01 +0700

    logo

--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -4,14 +4,15 @@ import {Button, Layout, Menu} from "antd";
 import { Link } from "react-router-dom";
 import {PAGE} from "../choose-package/ChoosePackagePage";
 import React from "react";
+import logo from "../../logo.svg"
 
 const { Header } = Layout;
 
 function NavHeader() {
   return (
     <Header className="bg-yellow-400 pd-1">
-        <div className="flex justify-between items-center">
-            <h1>Logo</h1>
+        <div className="flex justify-between items-center h-full">
+            <img className="w-2/6 " src={logo} alt="FoodAround"/>
             <Button
                 className="bg-yellow-400 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
                 onClick={
