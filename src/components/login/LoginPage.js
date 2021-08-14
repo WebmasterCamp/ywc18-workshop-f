576315dce267041f602c12731e0ff1fde5f135a6
Intouch Yutinatorn <keyeducation2544@gmail.com>
2021-08-14 11:15:06 +0700

    login

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { Button } from "antd";
-import { useHistory } from "react-router-dom";
+import {Button, Form, Input} from "antd";
+import {useHistory} from "react-router-dom";
 
-export const LoginPage = ({ setIsLogin }) => {
-  const history = useHistory();
-  return (
-    <div className="w-full h-full">
-      <div className="bg-white mx-auto">
-        <Button
-          onClick={() => {
-            setIsLogin(true);
-            localStorage.setItem("isLogin", true);
-            history.push("/");
-          }}
-        >
-          login
-        </Button>
-      </div>
-    </div>
-  );
+export const LoginPage = ({setIsLogin}) => {
+    const [form] = Form.useForm()
+    const history = useHistory();
+    return (
+        <div className="w-full h-full text-2xl">
+            <div className="bg-white mx-auto p-5">
+                Login
+                <div className="mb-8 mt-8">
+                    <Form form={form}>
+                        <Form.Item name="email">
+                            <Input type="email" placeholder="Email address"/>
+                        </Form.Item>
+                        <Form.Item name="password">
+                            <Input type="password" placeholder="Password"/>
+                        </Form.Item>
+                    </Form>
+                    <Button
+                        onClick={() => {
+                            setIsLogin(true);
+                            localStorage.setItem("isLogin", true);
+                            history.push("/");
+                        }}
+                    >
+                        next
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
