64cdbeb8bb6838ad26dce5ea36965b482d07fead
Aek <ekkawin.v@gmail.com>
2021-08-14 17:21:29 +0700

    button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import NavHeader from "./components/common/NavHeader";
 import WebsiteFooter from "./components/common/WebsiteFooter";
 import HomePage from "./components/home/HomePage";
 
-import { Layout } from "antd";
-import { Switch, Route, useHistory } from "react-router-dom";
+/** @jsxImportSource @emotion/react */
+import { Layout, Button } from "antd";
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { LoginPage } from "./components/login/LoginPage";
 import { RegisterPage } from "./components/login/RegisterPage";
 import { ChoosePackagePage } from "./components/choose-package/ChoosePackagePage";
+import { css } from "@emotion/react";
 
 const App = () => {
   const history = useHistory();
+  const location = useLocation();
+
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
@@ -24,7 +28,7 @@ const App = () => {
 
   useEffect(() => {
     if (!isLogin) {
-      history.push("/login");
+      history.push("/home");
     } else {
       setIsLogin(true);
     }
@@ -32,7 +36,7 @@ const App = () => {
 
   if (!isLogin) {
     return (
-      <>
+      <div>
         <Route exact path="/login">
           <LoginPage setIsLogin={setIsLogin} />
         </Route>
@@ -40,9 +44,30 @@ const App = () => {
           <RegisterPage setIsLogin={setIsLogin} />
         </Route>
         <Route exact path="/home">
-          <div>aek</div>
+          <div className="w-full h-full relative">
+            <img
+              className="relative mx-auto"
+              src="assets/X - 28.png"
+              css={css`
+                max-width: 475px !important;
+              `}
+            />
+
+            <Button
+              onClick={() => {
+                history.push("/login");
+              }}
+              className="absolute w-64 h-16 bg-yellow-400 border-2 border-black rounded-lg hover:bg-yellow-600 hover:border-none"
+              css={css`
+                top: 380px;
+                left: 100px;
+              `}
+            >
+              สมัครเลย
+            </Button>
+          </div>
         </Route>
-      </>
+      </div>
     );
   }
 
@@ -50,14 +75,37 @@ const App = () => {
     <>
       <Switch>
         <Layout className="layout">
-          <NavHeader />
+          {location?.pathname !== "/home" && <NavHeader />}
           <Route exact path="/">
             <HomePage />
           </Route>
           <Route exact path="/create-package">
             <ChoosePackagePage />
           </Route>
-          <WebsiteFooter />
+          <Route exact path="/home">
+            <div className="w-full h-full relative">
+              <img
+                className="relative mx-auto"
+                src="assets/X - 28.png"
+                css={css`
+                  max-width: 475px !important;
+                `}
+              />
+
+              <Button
+                onClick={() => {
+                  history.push("/");
+                }}
+                className="absolute w-64 h-16 bg-yellow-400 border-2 text-black border-black rounded-lg hover:bg-yellow-600 hover:border-none hover:text-black"
+                css={css`
+                  top: 380px;
+                  left: 100px;
+                `}
+              >
+                หน้าหลัก
+              </Button>
+            </div>
+          </Route>
         </Layout>
       </Switch>
     </>
