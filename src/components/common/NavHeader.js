26dee84d05a93a15f26a56024ec2d10577b0793f
Intouch Yutinatorn <keyeducation2544@gmail.com>
2021-08-14 16:43:06 +0700

    Ant icon

--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -1,30 +1,32 @@
 import "antd/dist/antd.css";
 
-import { Button, Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
-import { PAGE } from "../choose-package/ChoosePackagePage";
+import {Button, Layout, Menu} from "antd";
+import {LogoutOutlined} from '@ant-design/icons';
+import {Link} from "react-router-dom";
+import {PAGE} from "../choose-package/ChoosePackagePage";
 import React from "react";
 import logo from "../../logo.svg";
 
-const { Header } = Layout;
+const {Header} = Layout;
 
 function NavHeader() {
-  return (
-    <Header className="bg-yellow-400 pd-1">
-      <div className="flex justify-between items-center h-full">
-        <img className="w-2/6 " src={logo} alt="FoodAround" />
-        <Button
-          className="bg-yellow-400 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
-          onClick={() => {
-            localStorage.setItem("isLogin", false);
-            window.location.href = "/";
-          }}
-        >
-          Logout
-        </Button>
-      </div>
-    </Header>
-  );
+    return (
+        <Header className="bg-yellow-400 pd-1">
+            <div className="flex justify-between items-center h-full">
+                <img className="w-2/6 " src={logo} alt="FoodAround"/>
+                <Button
+                    icon={<LogoutOutlined />}
+                    className="bg-yellow-500 hover:bg-color-600 cursor-pointer text-white border-black rounded-lg hover:bg-yellow-600"
+                    onClick={() => {
+                        localStorage.setItem("isLogin", false);
+                        window.location.href = "/";
+                    }}
+                >
+                    Logout
+                </Button>
+            </div>
+        </Header>
+    );
 }
 
 export default NavHeader;
